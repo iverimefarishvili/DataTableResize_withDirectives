bf78e7bcabf8140ee89025e25de59d664cf05d82
iverimefarishvili <mefarishvili922@gmail.com>
2019-09-27 15:48:36 +0400

    add

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostListener } from '@angular/core';
+import { Component, HostListener, ViewChildren, QueryList, ElementRef, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -9,6 +9,15 @@ export class AppComponent {
   mouseStart = 0;
   mouseEnd = 0;
   isactive = false;
+
+  @ViewChildren('el1')
+  el1: QueryList<ElementRef>;
+
+  @ViewChildren('targetTh')
+  el2: QueryList<ElementRef>;
+  
+  constructor(private renderer: Renderer2) {};
+
   @HostListener('document:mousemove', ['$event'])
   onMouseMove(e) {
     this.mouseEnd = e.x;
@@ -21,8 +30,19 @@ export class AppComponent {
 
   onResizeEnd(event) {
     if(this.isactive) {
-      console.log(this.mouseEnd - this.mouseStart);
+      let difference = this.mouseEnd - this.mouseStart;
       this.isactive = false;
+
+      let widths = this.el1.map(th => th.nativeElement.offsetWidth);
+      console.log(widths[0]);
+      widths = difference + widths[0];
+      this.el1.forEach((th, index) => {
+        this.renderer.setStyle(
+          th.nativeElement, 
+          'width', 
+          `${widths}px`
+        );
+      });
     }
   }
 }
