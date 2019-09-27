b3dfb46ff52e1599a40d8c9ccf56885aa4b4c0af
iverimefarishvili <mefarishvili922@gmail.com>
2019-09-27 16:12:16 +0400

    add

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -9,15 +9,13 @@ export class AppComponent {
   mouseStart = 0;
   mouseEnd = 0;
   isactive = false;
+  
 
   @ViewChildren('el1')
   el1: QueryList<ElementRef>;
 
   @ViewChildren('el2')
   el2: QueryList<ElementRef>;
-
-  @ViewChildren('el3')
-  el3: QueryList<ElementRef>;
   
   constructor(private renderer: Renderer2) {};
 
@@ -25,27 +23,40 @@ export class AppComponent {
   onMouseMove(e) {
     this.mouseEnd = e.x;
   }
+  element = 1;
 
-  onResizeStart(event) {
+  onResizeStart(event, element) {
+    this.element = element;
     this.mouseStart = event.x;
     this.isactive = true;
   }
 
-  onResizeEnd(event, element) {
+  onResizeEnd(event) {
     if(this.isactive) {
       let difference = this.mouseEnd - this.mouseStart;
       this.isactive = false;
 
-      let widths = this.el1.map(th => th.nativeElement.offsetWidth);
-      console.log(widths[0]);
-      widths = difference + widths[0];
-      this.el1.forEach((th, index) => {
-        this.renderer.setStyle(
-          th.nativeElement, 
-          'width', 
-          `${widths}px`
-        );
-      });
+      if(this.element == 1) {
+        let widths = this.el1.map(th => th.nativeElement.offsetWidth);
+        widths = difference + widths[0];
+        this.el1.forEach((th, index) => {
+          this.renderer.setStyle(
+            th.nativeElement, 
+            'width', 
+            `${widths}px`
+          );
+        });
+      } else {
+        let widths = this.el2.map(th => th.nativeElement.offsetWidth);
+        widths = difference + widths[0];
+        this.el2.forEach((th, index) => {
+          this.renderer.setStyle(
+            th.nativeElement, 
+            'width', 
+            `${widths}px`
+          );
+        });
+      }
     }
   }
 }
